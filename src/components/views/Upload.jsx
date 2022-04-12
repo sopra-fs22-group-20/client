34657a808e615e52965e86c9465f145a9db0c82f
L-Huber <71383238+L-Huber@users.noreply.github.com>
2022-04-12 14:12:30 +0200

    Update Upload.jsx
    this fixes #50 and #53
    -thx Melih!
    -created basic upload function to firebase, so an image can be chosen and uploaded
    -format of the name of the image is done like: id_timestamp_NameOfImage

--- src/components/views/Upload.jsx
+++ src/components/views/Upload.jsx
@@ -1,12 +1,55 @@
 import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import styled from 'styled-components';
+// import for ref in firebase, getDownloadURL return url to access the image
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { api, handleError } from '../../helpers/api';
 import FormField from './FormField';
 import { Button } from '../ui/Button';
 import BaseContainer from '../ui/BaseContainer';
+import { storage } from './firebase';
 
+function uploadFile(file) {
+  // defines the name of the image => uploaded image.png stays the same
+  const fileName = file.name;
+  // will put the file to images, the name is given by the userId and the timestamp
+  const timestamp = Date.now().toString();
+  console.log('Time of upload', timestamp);
+  // get id out of local storage
+  const id = localStorage.getItem('id');
+  // create the timestamp in this format: id_timestamp_NameOfPicture
+  const fileNameToStore = id.concat('_', timestamp.toString());
+  const fileNameToStoreTimestamp = fileNameToStore.concat('_', fileName.toString());
+  console.log('fileNameToStore:', fileNameToStoreTimestamp);
+  // store in storage
+  const fileRef = ref(storage, `images/${fileNameToStoreTimestamp}`);
+
+  uploadBytes(fileRef, file).then(
+
+    // successfully uploaded
+
+    (snapshot) => {
+      getDownloadURL(snapshot.ref).then((downloadURL) => {
+        // write this to database
+        // return URL of the image
+
+        console.log('File available at', downloadURL);
+      });
+
+      console.log('Uploaded a blob or file!');
+      // Store the new downloadURL together with credentials in Databse:
+    },
+
+    // error
+
+    (error) => {
+      console.log(`error => ${error}`);
+    },
+
+  );
+}
 function Upload() {
+  const [selectedFile, setFile] = useState(null);
   const history = useHistory();
   const [title, setTitle] = useState('');
   const [category, setCategory] = useState('');
@@ -21,7 +64,7 @@ function Upload() {
         location,
       });
 
-      const response = await api.post('/pictures', requestBody);
+      const response = await api.post('/upload', requestBody);
 
       // Get the returned user and update a new object.
 
@@ -35,44 +78,21 @@ function Upload() {
     <div>
       <h2>Upload</h2>
       <p>On this page, you can upload a new picture to RateMe!</p>
-      <BaseContainer>
-        <div className="upload container">
-          <div
-            className="upload form"
-            style={{
-              height: '800px',
-            }}
-          >
-            <FormField
-              label="Title"
-              value={title}
-              onChange={(un) => setTitle(un)}
-            />
-            <FormField
-              label="category"
-              value={category}
-              onChange={(n) => setCategory(n)}
-            />
-            <FormField
-              label="location"
-              value={location}
-              onChange={(n) => setLocation(n)}
-            />
-            <div className="login button-container">
-              <Button
-                disabled={!title || !category || !location}
-                width="100%"
-                onClick={() => doUpload()}
-              >
-                Upload!
-              </Button>
-            </div>
-          </div>
-          {
-                    // adds link element/component under the login component
-                }
-        </div>
-      </BaseContainer>
+      <h2>Upload image</h2>
+
+      <input
+        type="file"
+        onChange={(e) => setFile(e.target.files[0])}
+        multiple={false}
+      />
+      <button
+        onClick={() => {
+          uploadFile(selectedFile);
+        }}
+      >
+        Upload
+      </button>
+
     </div>
   );
 }
